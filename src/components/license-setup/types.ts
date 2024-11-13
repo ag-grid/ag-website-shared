36598097fb3f9a2dd9b4404d0ca3f5aac7aee0e8
Tak Tran <contact@tutaktran.com>
2024-11-13 16:36:48 +0000

    AG-3390 - Unshare license setup from ag-website-shared
    To allow the folder to be synced with charts.

--- src/components/license-setup/types.ts
+++
@@ -1,8 +0,0 @@
-import type { LicenseManager } from '@ag-grid-enterprise/core';
-
-export interface LicensedProducts {
-    grid: boolean;
-    charts: boolean;
-}
-
-export type LicenseDetails = ReturnType<typeof LicenseManager.getLicenseDetails>;
